d3a5c8d5056b66c7ee8e3231147a26f412213578
Andrew Graham <andrew@firehawkvfx.com>
2020-11-07 19:38:17 +1030

    use include with sync command

--- examples/nice-dcv-ami/build.sh
+++ examples/nice-dcv-ami/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-aws s3 sync s3://ec2-linux-nvidia-drivers/latest/ /tmp/.
+aws s3 sync s3://ec2-linux-nvidia-drivers/latest/ /tmp/. --include "NVIDIA-Linux-x86_64-*-grid-aws.run"
 export PKR_VAR_nvidia_driver=$(ls /tmp/NVIDIA-Linux-x86_64-*-grid-aws.run | tail -1)
 packer build nice-dcv.json.pkr.hcl 
