bfbba836de5a776f1fffe7c62cf26866052f090b
Andrew Graham <andrew@firehawkvfx.com>
2020-11-07 22:50:46 +1030

    remove reboot status

--- examples/nice-dcv-ami/build.sh
+++ examples/nice-dcv-ami/build.sh
@@ -8,7 +8,7 @@ export AWS_DEFAULT_REGION=$(curl -s http://169.254.169.254/latest/meta-data/plac
 export PKR_VAR_aws_region="$AWS_DEFAULT_REGION"
 if [[ -f "../bastion-ami/manifest.json" ]]; then
     export PKR_VAR_bastion_centos7_ami="$(jq -r '.builds[] | select(.name == "centos7-ami") | .artifact_id' ../bastion-ami/manifest.json | tail -1 | cut -d ":" -f2)"
-    echo "Found bastion_cento7_ami in manifest: PKR_VAR_bastion_centos7_ami=$PKR_VAR_bastion_centos7_ami"
+    echo "Found bastion_centos7_ami in manifest: PKR_VAR_bastion_centos7_ami=$PKR_VAR_bastion_centos7_ami"
 fi
 export PACKER_LOG=1
 export PACKER_LOG_PATH="packerlog.log"
--- examples/nice-dcv-ami/nice-dcv.json.pkr.hcl
+++ examples/nice-dcv-ami/nice-dcv.json.pkr.hcl
@@ -198,8 +198,6 @@ EOFO
     inline = [
       "set -x; ls -ltriah /tmp/nvidia; sudo chmod +x ${var.nvidia_driver}",
       "ls -ltriah /tmp/nvidia", # Check exec permissions
-      # "sudo init 3", # Stop x server
-      "systemctl status reboot",
       "sudo systemctl get-default",
       "sudo systemctl isolate multi-user.target",
       "sudo /bin/sh ${var.nvidia_driver} --dkms -s --install-libglvnd"
