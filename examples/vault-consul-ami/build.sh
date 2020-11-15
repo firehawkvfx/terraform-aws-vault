f8e8632f5d3955653dccb31ca57674ba795c3062
Andrew Graham <andrew@firehawkvfx.com>
2020-11-15 14:04:40 +1030

    build hcl

--- examples/vault-consul-ami/build.sh
+++ examples/vault-consul-ami/build.sh
@@ -10,4 +10,4 @@ export PACKER_LOG=1
 export PACKER_LOG_PATH="$SCRIPTDIR/packerlog.log"
 
 rm -f $SCRIPTDIR/manifest.json
-packer build $SCRIPTDIR/vault-consul.json | ts '[%H:%M:%S]'
+packer build $SCRIPTDIR/vault-consul.json.pkr.hcl | ts '[%H:%M:%S]'
